bc70a0e2462dadcf056eb2054cfeb48e058dc77f
JiwooL0920 <JiwooL0920@gmail.com>
2021-05-24 22:37:30 -0400

    05-24-2021

---
+++ trees/easy/[112]path-sum.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        if not root:
+            return False 
+        elif not root.left and not root.right:
+            return root.val == targetSum 
+        else:
+            newSum = targetSum - root.val
+            return self.hasPathSum(root.left, newSum) or self.hasPathSum(root.right, newSum)
+            
+        
+            
+    
