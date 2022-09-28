c1fb76faa3231d36ee83f56d89ccbde8265c505d
JiwooL0920 <JiwooL0920@gmail.com>
2022-09-28 19:51:14 -0400

    update

---
+++ NeetCode/1. Arrays & Hashing/practice/[E.1304] Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,35 @@
+# Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+# Example 1:
+# Input: n = 5
+# Output: [-7,-1,1,3,4]
+# Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+# Example 2:
+# Input: n = 3
+# Output: [-1,0,1]
+
+# Example 3:
+# Input: n = 1
+# Output: [0]
+
+class Solution(object):
+    def sumZero(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+#         5
+#         [-2,-1,0,1,2]
+        
+#         6
+#         [-3,-2,-1,1,2,3]
+
+        res = [] if n % 2 == 0 else [0]
+        bound = n // 2 
+        while bound:
+            res.append(-bound)
+            res.append(bound)
+            bound -= 1 
+            
+        return res
