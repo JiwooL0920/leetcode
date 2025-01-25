172a66fffa44a631110e9d015fdb641844626fab
jiwool0920 <jiwool0920@gmail.com>
2025-01-25 18:02:38 -0500

    Sat Jan 25 18:02:37 EST 2025

---
+++ 2025/2_TwoPointers/[M.167] Two Sum II - Input Array is Sorted.py
@@ -0,0 +1,16 @@
+# 01/25/2025
+# Time: O(N), Space: O(1)
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            summ = numbers[l] + numbers[r]
+            if summ < target:
+                l += 1
+            elif summ > target:
+                r -= 1
+            else:
+                # indices are based on 1, so add 1
+                return [l+1, r+2] 
+        return [] 
