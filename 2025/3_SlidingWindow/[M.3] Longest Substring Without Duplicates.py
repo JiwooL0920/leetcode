7f92c0ff306134cc6bc77738bb98e0dd0d90bd60
jiwool0920 <jiwool0920@gmail.com>
2025-02-01 14:53:29 -0500

    Sat Feb  1 14:53:29 EST 2025

---
+++ 2025/3_SlidingWindow/[M.3] Longest Substring Without Duplicates.py
@@ -0,0 +1,22 @@
+# Feb 1, 2025
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+# Time: O(N)
+# Space: O(N)
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        unique = set()
+        res = 0
+        l = 0
+        for r in range(len(s)):
+            # shrink left window until there is no duplicate
+            while s[r] in unique:
+                unique.remove(s[l])
+                l += 1
+            # expand right window while no duplicate is found
+            res = max(res, r-l+1)
+            unique.add(s[r])
+        return res
+            
+
