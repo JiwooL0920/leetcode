c1c243f6b241703639126ecdefac6ad230571563
jiwool0920 <jiwool0920@gmail.com>
2025-02-02 21:43:26 -0500

    Sun Feb  2 21:43:25 EST 2025

--- 2025/3_SlidingWindow/Blind75/[H.76] Minimum Window Substring.py
+++ 2025/3_SlidingWindow/Blind75/[H.76] Minimum Window Substring.py
@@ -4,41 +4,64 @@
 # Given two strings s and t, return the minimum window in s which will contain all the characters in t. If there is no such window in s that covers all characters in t, return the empty string ""
 # Note that If there is such a window, it is guaranteed that there will always be only one unique minimum window in s.
 
+# one variable "have" to represent the total num of characters we have out of the target
+# need: remaining charcter to get target
+# once we find a substring (have == need), keep shrinking left window until have < need
+# repeat the process to see if there is a smaller substring
+
+# Time: O(N)
+# Space: O(N)
 class Solution:
-    def minWindow(self, s:str, t:str) -> str:
+    def minWindow(self, s: str, t: str) -> str:
         if t == "": return ""
 
-        countT, windwow = {}, {}
+        # count for unique characters in t
+        countT = {}
+        # track the number of characters in the current window
+        window = {}
 
         # initialize countT
         for c in t:
             countT[c] = 1 + countT.get(c, 0)
 
         have, need = 0, len(countT)
-        res, resLen = [-1, -1], float('infinity')
+        res, resLen = [-1, -1], float("infinity")
 
         l = 0
+        # start expanding the right window
         for r in range(len(s)):
             c = s[r]
+            # update number of characters in the current window
             window[c] = 1 + window.get(c, 0)
 
-            # just satisfied for condition for the first time
-            if c in countT and window[c] == countT:
+            # see if we found a target character
+            if (
+                c in countT # c is target character
+                and window[c] == countT[c] # required number of c required in t is met
+            ):
                 have += 1
 
+            # met the requirements for target string
             while have == need:
-                # update our result
+                # found a new candidate, see if we can update min window
                 if (r-l+1) < resLen:
                     res = [l, r]
-                    resLen = r-l+1
-                # keep shrinking from the left
-                # pop from the left of our winddow
+                    resLen = (r-l+1)
+
+                # pop from the left of our window
                 window[s[l]] -= 1
-                # its possible that have and need condition is broken
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+
+                # update have count if we need to
+                # when we move l to shrink the window, we are removing s[l] from the current window
+                # if s[l] is a target character
+                # and we remove one occurrence below the required count, we must decrease have
+                if (
+                    s[l] in countT # it was a target character
+                    and window[s[l]] < countT[s[l]] # count of char in current window went below number of chars required in target
+                ):
                     have -= 1
+                # shift left window by 1
                 l += 1
 
         l, r = res
-        return s[l:r+1] if resLen != float('infinity') else ""
-
+        return s[l:r+1] if resLen != float("infinity") else ""
