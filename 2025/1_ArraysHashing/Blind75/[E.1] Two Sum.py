3c422b3ca1e95440a36f8ae2741b140e5d02ff95
jiwool0920 <jiwool0920@gmail.com>
2025-02-06 22:15:53 -0500

    Thu Feb  6 22:15:52 EST 2025

--- 2025/1_ArraysHashing/Blind75/[E.1] Two Sum.py
+++ 2025/1_ArraysHashing/Blind75/[E.1] Two Sum.py
@@ -5,12 +5,12 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Hashmap
         # Time: O(N), Space: O(N)
-        
+
         # 1) record val:index for each number in the list
         indices = {} # val : index
         for i, n in enumerate(nums):
             indices[n] = i
-        
+
         # 2) iterate through the list again, checking if the value for target-n is there
         for i, n in enumerate(nums):
             diff = target - n
@@ -19,3 +19,17 @@ class Solution:
                 and indices[diff] != i
             ):
                 return [i, indices[diff]]
+
+# -----------------------------------------------------------------------
+# Review: Feb 6, 2025
+# Time: O(N), Space: O(N)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mapp = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in mapp:
+                return [mapp[diff], i]
+            mapp[n] = i
+
