4a787694eb927e3cb99c59949eae84c4e4183757
Jiwoo Lee <jiwool0920@gmail.com>
2025-02-09 05:01:22 +0000

    Sun Feb  9 05:01:22 UTC 2025

--- 2025/1_ArraysHashing/Blind75/[M.128] Longest Consecutive Sequence.py
+++ 2025/1_ArraysHashing/Blind75/[M.128] Longest Consecutive Sequence.py
@@ -18,3 +18,24 @@ class Solution:
                 longest = max(length, longest)
         
         return longest
+
+# -----------------------------------------------------------------------
+# Review: Feb 8, 2025
+# Wrong answer, but if question were to find the longest consequtive sequence existing
+# Time: O(N), Space: O(1)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0 
+
+        i = 0
+        while i < len(nums):
+            streak = 1
+            j = i+1
+            while j < len(nums) and nums[j]-nums[i] == 1:
+                streak += 1
+                j += 1
+                i += 1
+            res = max(res, streak)
+            i = j+1
+        
+        return res 
