ef54b0903d6e24b4571596f212d36fb05ebc7d75
Jiwoo Lee <jiwool0920@gmail.com>
2025-02-12 03:48:00 +0000

    Wed Feb 12 03:48:00 UTC 2025

--- 2025/2_TwoPointers/Blind75/[M.11] Container With Most Water.py
+++ 2025/2_TwoPointers/Blind75/[M.11] Container With Most Water.py
@@ -21,3 +21,24 @@ class Solution:
                 r -= 1
         
         return max_area
+
+# -----------------------------------------------------------------------
+# Review: Feb 11, 2025
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        res = 0
+        l, r = 0, len(heights)-1
+
+        while l < r:
+            width = r-l
+            hl, hr = heights[l], heights[r]
+            height = min(hl, hr)
+            area = width * height
+            res = max(res, area)
+
+            if hl <= hr:
+                l += 1 
+            else:
+                r -= 1
+            
+        return res 
--- 2025/2_TwoPointers/Neetcode150/[M.167] Two Sum II - Input Array is Sorted.py
+++ 2025/2_TwoPointers/Neetcode150/[M.167] Two Sum II - Input Array is Sorted.py
@@ -14,3 +14,18 @@ class Solution:
                 # indices are based on 1, so add 1
                 return [l+1, r+1] 
         return [] 
+
+# -----------------------------------------------------------------------
+# Review: Feb 8, 2025
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            n1, n2 = numbers[l], numbers[r]
+            summ = n1 + n2
+            if summ < target:
+                l += 1
+            elif summ > target:
+                r -= 1
+            else:
+                return [l+1, r+1]
