ad3c3612bcdbc9b1f892633a8a2b5c8ca348ebeb
Jiwoo Lee <jiwool0920@gmail.com>
2025-02-12 03:06:50 +0000

    Wed Feb 12 03:06:50 UTC 2025

--- 2025/2_TwoPointers/Blind75/[E.125] Valid Palindrome.py
+++ 2025/2_TwoPointers/Blind75/[E.125] Valid Palindrome.py
@@ -24,3 +24,27 @@ class Solution:
             j -= 1
         return True
 
+# -----------------------------------------------------------------------
+# Review: Feb 11, 2025
+class Solution:
+    def isAlphaNum(self, c: str):
+        return (
+           ord('a') <= ord(c) <= ord('z')
+           or ord('0') <= ord(c) <= ord('9')  
+        )
+
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l <= r:
+            cl, cr = s[l].lower(), s[r].lower()
+            if not self.isAlphaNum(cl):
+                l += 1
+            elif not self.isAlphaNum(cr):
+                r -= 1
+            else:
+                if cl != cr:
+                    return False
+                l += 1
+                r -= 1
+        return True
+        
