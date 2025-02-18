f64dcfd469f7aecdfe8e69be956b1d495d88e153
jiwool0920 <jiwool0920@gmail.com>
2025-02-17 20:28:11 -0500

    Mon Feb 17 20:28:10 EST 2025

---
+++ 2025/3_SlidingWindow/Neetcode250/[E.1768] Merge Strings Alternately.py
@@ -0,0 +1,19 @@
+# Feb 17, 2025
+# https://leetcode.com/problems/merge-strings-alternately/description/
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ""
+        i = 0
+        len1, len2 = len(word1), len(word2)
+        while i < len1 or i < len2:
+            if i < len1:
+                result += word1[i]
+            if i < len2:
+                result += word2[i]
+            i += 1
+        return result
+
